46c8ed8a9f5fe48c9cc3166badcce8390a995242
dkav13 <karneyrita@gmail.com>
2020-01-31 11:55:39 +0300

    Added Log.cs

---
+++ WebApplication4/data/Models/Log.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace data.Models
+{
+    class Log
+    {
+        public DateTime Date { get; set; }
+        public int LeadId { get; set; }
+        public Lead Lead { get; set; }
+    }
+}
