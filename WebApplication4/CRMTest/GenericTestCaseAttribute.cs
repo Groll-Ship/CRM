bcb98a03c24b40276cfff3152cb86fabfa856b26
MihaBukaev <miha.bukaev@gmail.com>
2020-02-02 16:24:28 +0300

    add GenericTestCase

---
+++ WebApplication4/CRMTest/GenericTestCaseAttribute.cs
@@ -0,0 +1,30 @@
+﻿
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using System;
+using System.Collections.Generic;
+
+namespace CRMTest
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class GenericTestCaseAttribute : TestCaseAttribute, ITestBuilder
+    {
+        private readonly Type _type;
+        public GenericTestCaseAttribute(Type type) : 
+            base()
+        {
+            _type = type;
+        }
+
+        IEnumerable<TestMethod> ITestBuilder.BuildFrom(IMethodInfo method, Test suite)
+        {
+            if (method.IsGenericMethodDefinition && _type != null)
+            {
+                var gm = method.MakeGenericMethod(_type);
+                return BuildFrom(gm, suite);
+            }
+            return BuildFrom(method, suite);
+        }
+    }
+}
