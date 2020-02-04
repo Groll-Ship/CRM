a931576ecc0eef7ccfb825e538263d71d2758b00
MihaBukaev <miha.bukaev@gmail.com>
2020-02-05 00:01:16 +0300

    add method for AdminManager

--- WebApplication4/ClassLibrary1/AdminManager.cs
+++ WebApplication4/ClassLibrary1/AdminManager.cs
@@ -1,20 +1,56 @@
 ﻿using busines.Interface;
 using data.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 
 namespace busines
 {
     public class AdminManager: UserManager
     {
+
         #region GET 
 
-
+        public IEnumerable<IEntity> GetAll<T>(params IEntity[] entity) where T: new ()
+        {
+            return _storage.GetAll<T>(entity);
+        }
 
         #endregion
 
         #region CREAT 
 
-        //creat HR
+        public bool CreatLead(object obj, Group group, Status status, Course course)
+        {
+            
+            string str = obj.ToString();
+            var dictionary = JsonConvert.DeserializeObject<Dictionary<string, string>>(str);
+
+            Lead lead = new Lead(){
+                FName = (dictionary.ContainsKey("FName")) ? dictionary["FName"] : "",
+                SName = (dictionary.ContainsKey("SName")) ? dictionary["SName"] : "",
+                DateBirthday = (dictionary.ContainsKey("DateBirthday")) ? dictionary["DateBirthday"] : "",
+                DateRegistration = DateTime.Now.ToString(),
+                Numder = (dictionary.ContainsKey("Numder")) ? StringToInt(dictionary["Numder"]) : 0,
+                EMail = (dictionary.ContainsKey("EMail")) ? dictionary["EMail"] : "",
+                AccessStatus = false,
+                //NameGroup = group.NameGroup,
+                Group = group,
+                //CourseId = course.Id,
+                Course = course,
+                //StatusId = status.Id,
+                Status = status,
+            };
+            _storage.Add(lead);
+
+            History history = new History(){
+                Lead = lead,
+                HistoryText = DateTime.Now.ToString() + "  Студент зарегестрирован.",
+            };
+            _storage.Add(history);
+
+            return true;
+        }
         
         #endregion
 
@@ -48,6 +84,10 @@ namespace busines
         public bool DeleteTeacher(Teacher teacher) { return _storage.Delete(teacher); }
 
         #endregion
+        private int StringToInt(string str)
+        {
+            return int.TryParse(str, out int number) ? number : 0;
+        }
 
     }
 }
