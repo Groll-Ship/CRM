74fcb334edce8b0cfb4ecbc8296e2a301aa2c5a1
filin0wl <nastia-dogf@mail.ru>
2020-02-09 19:57:00 +0300

    Add Cache HR and Teachers

---
+++ WebApplication4/ClassLibrary1/Cache/CacheHR.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using data;
+using data.Models;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace busines
+{
+    class CacheHR
+    {
+        public List<Lead> Leads { get; set; }
+        public List<Course> Courses { get; set; }
+        public List<Group> Groups { get; set; }
+        public List<History> Histories { get; set; }
+        public List<HistoryGroup> HistoryGroups { get; set; }
+        public List<HR> HRs { get; set; }
+        public List<Teacher> Teachers { get; set; }
+        public List<Skills> Skills { get; set; }
+        public List<SkillsLead> SkillsLeads { get; set; }
+        public List<Status> Statuses { get; set; }
+
+        public CacheHR ()
+        {
+            this.Set();
+        }
+
+        private void Set()
+        {
+            Storage _storage = new Storage();
+            Leads = (List<Lead>)_storage.GetAll<Lead>();
+            Courses = (List<Course>)_storage.GetAll<Course>();
+            Groups = (List<Group>)_storage.GetAll<Group>();
+            Histories = (List<History>)_storage.GetAll<History>();
+            HistoryGroups = (List<HistoryGroup>)_storage.GetAll<HistoryGroup>();
+            HRs = (List<HR>)_storage.GetAll<HR>();
+            Teachers = (List<Teacher>)_storage.GetAll<Teacher>();
+            Skills = (List<Skills>)_storage.GetAll<Skills>();
+            SkillsLeads = (List<SkillsLead>)_storage.GetAll<SkillsLead>();
+            Statuses = (List<Status>)_storage.GetAll<Status>();
+
+        }
+
+    }
+
+
+}
---
+++ WebApplication4/ClassLibrary1/Cache/CacheTeacher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using data;
+using data.Models;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace busines.Cache
+{
+    class CacheTeacher
+    {
+        public Teacher concreatTeacher { get; set; }
+        public List<Lead> Leads { get; set; }
+        public List<Course> Courses { get; set; }
+        public List<Group> Groups { get; set; }
+        public List<History> Histories { get; set; }
+        public List<HistoryGroup> HistoryGroups { get; set; }
+        public List<Log> Logs { get; set; }
+        public List<Skills> Skills { get; set; }
+        public List<SkillsLead> SkillsLeads { get; set; }
+        
+        public CacheTeacher (Teacher teacher)
+        {
+            concreatTeacher = teacher;
+            this.Set();
+        }
+
+        private void Set()
+        {
+            Storage _storage = new Storage();
+            Groups = (List<Group>)_storage.GetAll<Group>(concreatTeacher.Id);
+            Leads = (List<Lead>)_storage.GetAll<Lead>(Groups);
+            Courses = (List<Course>)_storage.GetAll<Course>(Groups);
+            Histories = (List<History>)_storage.GetAll<History>(Leads);
+            HistoryGroups = (List<HistoryGroup>)_storage.GetAll<HistoryGroup>(Groups);
+            Logs = (List<Log>)_storage.GetAll<Log>(Groups);
+            Skills = (List<Skills>)_storage.GetAll<Skills>();
+            SkillsLeads = (List<SkillsLead>)_storage.GetAll<SkillsLead>(Leads);
+        }
+    }
+}
